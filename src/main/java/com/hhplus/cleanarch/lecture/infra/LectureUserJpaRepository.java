4b58dbc494c226e38aa64ae1173671625519fb4c
Youngwon <yw7148@naver.com>
2024-06-23 13:27:49 +0900

    Revert "feat: 특강 신청/조회/내역 조회 API 개발"
    This reverts commit 362c46332b712c0a088228a3629805d1db911301.

--- src/main/java/com/hhplus/cleanarch/lecture/infra/LectureUserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hhplus.cleanarch.lecture.infra;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hhplus.cleanarch.lecture.entity.LectureUserEntity;
-
-public interface LectureUserJpaRepository extends JpaRepository<LectureUserEntity, Long>{
-    LectureUserEntity findByUserId(long userId);
-}
